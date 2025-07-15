d55c0efc38550f8cee0f8f1489a1033461cd3af3
Maja Nikoljic <108588361+majanikoljic@users.noreply.github.com>
2025-07-14 20:44:24 -0400

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(notFound);
 app.use(errorHandler);
 
 // Server Listener
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
